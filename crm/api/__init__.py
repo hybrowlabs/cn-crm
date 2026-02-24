65d5c6132f6bbd27c79c2d6749ac4dc97cd60c9a
shariquerik <sharique.rik@gmail.com>
2026-02-24 13:36:53 +0530

    fix: refactor import statement for get_modules_from_all_apps_for_user
    (cherry picked from commit a9ee69951bca89178bb44b3284b7b185e1dfd0fc)

--- crm/api/__init__.py
+++ crm/api/__init__.py
@@ -61,7 +61,9 @@ def check_app_permission():
 	if is_frappe_version("15"):
 		allowed_modules = frappe.config.get_modules_from_all_apps_for_user()
 	elif is_frappe_version("16", above=True):
-		allowed_modules = frappe.utils.modules.get_modules_from_all_apps_for_user()
+		from frappe.utils.modules import get_modules_from_all_apps_for_user
+
+		allowed_modules = get_modules_from_all_apps_for_user()
 
 	allowed_modules = [x["module_name"] for x in allowed_modules]
 	if "FCRM" not in allowed_modules:
